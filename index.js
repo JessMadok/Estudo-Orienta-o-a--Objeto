ae6e554c90a7754962f107751b6ce75c26f9ff14
JessMadok <jessicamadokoro@gmail.com>
2022-01-11 16:10:31 -0300

    Atributos, estáticos e encapsulamento

--- index.js
+++ index.js
@@ -1,43 +1,16 @@
+import { Cliente } from "./Cliente.js";
+import { ContaCorrente } from "./ContaCorrente.js";
 
-class Cliente{
-    nome;
-    cpf;
-    rg;
-}
-
-class ContaCorrente{
-    agencia;
-    _saldo = 0;
-
-    sacar(valor){
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
-        }
-    }
-
-    depositar(valor){
-        if(valor <= 0){
-            return;
-        }
-        this._saldo += valor;
-    }
-}
-
-const cliente1 = new Cliente();
-cliente1.nome = "Ricardo";
-cliente1.cpf = 11122233309;
-cliente1.rg = 123456789;
-
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 88822233309;
-cliente2.rg = 987654321;
-
-const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.agencia = 1001;
+const cliente1 = new Cliente("Ricardo", 11122233309, 123456789);
+const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
 contaCorrenteRicardo.depositar(100);
 
+const cliente2  = new Cliente("Alice", 88822233309, 987654321);
+const contaCorrenteAlice = new ContaCorrente(cliente2, 102);
+
+
 const valorSacado = contaCorrenteRicardo.sacar(50);
+
+contaCorrenteRicardo.transferir(200, contaCorrenteAlice);
 console.log(valorSacado);
 
